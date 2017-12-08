4c4418c54645a21663a6bfdf25b4a29ac4816e06
Nicole Fetalino <nicolefetalino@Administrators-Mac-mini.local>
2017-12-08 10:19:21 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { AppRegistry, ScrollView, Image, Text } from 'react-native';
+
+export default class App extends Component {
+  render() {
+      return (
+        <ScrollView>
+          <Text style={{fontSize:96}}>TEst</Text>
+          <Text style={{fontSize:96}}>TEst</Text>
+          <Text style={{fontSize:96}}>If you like</Text>
+         
+          <Text style={{fontSize:96}}>Scrolling down</Text>
+         
+          <Text style={{fontSize:96}}>What's the best</Text>
+        
+          <Text style={{fontSize:96}}>Framework around?</Text>
+         
+          <Text style={{fontSize:80}}>React Native</Text>
+        </ScrollView>
+    );
+  }
+}
+
+// skip these lines if using Create React Native App
+AppRegistry.registerComponent(
+  'AwesomeProject',
+  () => App);
+
